af2f0b30cb0477f0b0cf4eb718f2283c31373d2d
Nileshsvt <nileshsrivastava8948@gmail.com>
2024-07-16 03:47:44 +0530

    comment on posts

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 import {useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import LoadingSpinner from "./LoadingSpinner";
+import { formatPostDate } from "../../utils/date";
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
@@ -15,6 +16,13 @@ const Post = ({ post }) => {
 	const {data:authUser}=useQuery({queryKey:["authUser"]});
 
 	const queryClient=useQueryClient();
+
+	const postOwner = post.user;
+	const isLiked = post.likes.includes(authUser._id);
+
+	const isMyPost = authUser._id===post.user._id;
+
+	const formattedDate = formatPostDate(post.createdAt) ; 
 
 	const {mutate,isPending}= useMutation({
 		mutationFn: async ()=>{
@@ -68,14 +76,37 @@ const Post = ({ post }) => {
 		},
 	});
 
-	const postOwner = post.user;
-	const isLiked = post.likes.includes(authUser._id);
+	const { mutate: commentPost, isPending: isCommenting } = useMutation({
+		mutationFn: async () => {
+			try {
+				const res = await fetch(`/api/posts/comment/${post._id}`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ text: comment }),
+				});
+				const data = await res.json();
 
-	const isMyPost = authUser._id===post.user._id;
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess: () => {
+			toast.success("Comment posted successfully");
+			setComment("");
+			queryClient.invalidateQueries({ queryKey: ["posts"] });
+		},
+		onError: (error) => {
+			toast.error(error.message);
+		},
+	});
 
-	const formattedDate = "1h";
 
-	const isCommenting = false; 
 
 	const handleDeletePost = () => {
 		mutate();
@@ -83,6 +114,8 @@ const Post = ({ post }) => {
 
 	const handlePostComment = (e) => {
 		e.preventDefault();
+		if (isCommenting) return;
+		commentPost();
 	};
 
 	const handleLikePost = () => {
