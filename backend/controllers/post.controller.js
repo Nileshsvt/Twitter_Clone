b186da30232b42954a6f0c151785deee84ccb141
Nileshsvt <nileshsrivastava8948@gmail.com>
2024-07-16 03:33:47 +0530

    like post

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -112,7 +112,9 @@ export const likeUnlikePost=async(req,res)=>{
             //Unlike post
             await Post.updateOne({_id:postId},{$pull:{likes:userId}});
             await User.updateOne({_id:userId},{$pull:{likedPosts:postId}});
-            res.status(200).json({message:"Post unliked successfully"});
+
+            const updatedLikes=post.likes.filter((id)=>id.toString()!==userId.toString());
+            res.status(200).json(updatedLikes);
                
            }
            else{
@@ -128,7 +130,7 @@ export const likeUnlikePost=async(req,res)=>{
             })
 
             await notification.save();
-            res.status(200).json({message:"Post liked successfully"});
+            res.status(200).json(post.likes);
            }
 
       
--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -36,10 +36,40 @@ const Post = ({ post }) => {
 			toast.success("Post deleted successfully !");
 			queryClient.invalidateQueries({queryKey:["posts"]});
 		}
-	})
+	});
+
+	const { mutate: likePost, isPending: isLiking } = useMutation({
+		mutationFn: async () => {
+			try {
+				const res = await fetch(`/api/posts/like/${post._id}`, {
+					method: "POST",
+				});
+				const data = await res.json();
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess: (updatedLikes) => {
+			queryClient.setQueryData(["posts"], (oldData) => {
+				return oldData.map((p) => {
+					if (p._id === post._id) {
+						return { ...p, likes: updatedLikes };
+					}
+					return p;
+				});
+			});
+		},
+		onError: (error) => {
+			toast.error(error.message);
+		},
+	});
 
 	const postOwner = post.user;
-	const isLiked = false;
+	const isLiked = post.likes.includes(authUser._id);
 
 	const isMyPost = authUser._id===post.user._id;
 
@@ -55,7 +85,10 @@ const Post = ({ post }) => {
 		e.preventDefault();
 	};
 
-	const handleLikePost = () => {};
+	const handleLikePost = () => {
+		if (isLiking) return;
+		likePost();
+	};
 
 	return (
 		<>
@@ -148,7 +181,7 @@ const Post = ({ post }) => {
 										/>
 										<button className='btn btn-primary rounded-full btn-sm text-white px-4'>
 											{isCommenting ? (
-												<span className='loading loading-spinner loading-md'></span>
+												<LoadingSpinner size="md" />
 											) : (
 												"Post"
 											)}
@@ -164,14 +197,15 @@ const Post = ({ post }) => {
 								<span className='text-sm text-slate-500 group-hover:text-green-500'>0</span>
 							</div>
 							<div className='flex gap-1 items-center group cursor-pointer' onClick={handleLikePost}>
-								{!isLiked && (
+								{isLiking && <LoadingSpinner size='sm'/>}
+								{!isLiked && !isLiking && (
 									<FaRegHeart className='w-4 h-4 cursor-pointer text-slate-500 group-hover:text-pink-500' />
 								)}
-								{isLiked && <FaRegHeart className='w-4 h-4 cursor-pointer text-pink-500 ' />}
+								{isLiked && !isLiking && <FaRegHeart className='w-4 h-4 cursor-pointer text-pink-500 ' />}
 
 								<span
-									className={`text-sm text-slate-500 group-hover:text-pink-500 ${
-										isLiked ? "text-pink-500" : ""
+									className={`text-sm group-hover:text-pink-500 ${
+										isLiked ? "text-pink-500" : "text-slate-500"
 									}`}
 								>
 									{post.likes.length}
