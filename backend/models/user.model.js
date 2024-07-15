4c39b1fa803790815521e3d45870fba3d6886fe9
Nileshsvt <nileshsrivastava8948@gmail.com>
2024-07-15 18:14:07 +0530

    post routes and controllers

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -46,6 +46,11 @@ const userSchema= new mongoose.Schema({
         type:String,
         default:"",
     },
+    likedPosts:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Post",
+        default:[]
+}],
     
 },{timestamps:true});
 
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import {v2 as cloudinary} from "cloudinary";
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 
@@ -24,6 +25,7 @@ app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
+app.use("/api/posts",postRoutes);
 
 
 app.listen(8000,()=>{
