d959739db979d3f14df6b79b09bec136b8543c47
Nileshsvt <nileshsrivastava8948@gmail.com>
2024-07-15 18:18:53 +0530

    update in route file and paths

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,9 @@ import express from "express";
 import dotenv from 'dotenv';
 import {v2 as cloudinary} from "cloudinary";
 
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
-import postRoutes from "./routes/post.routes.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 
