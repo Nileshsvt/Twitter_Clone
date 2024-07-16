f28932bde2c4f2c90b3e6f94a1a1ee2c16ab1e72
Nileshsvt <nileshsrivastava8948@gmail.com>
2024-07-16 05:59:48 +0530

    prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from 'dotenv';
 import {v2 as cloudinary} from "cloudinary";
 
@@ -19,6 +20,8 @@ cloudinary.config({
 
 const app=express();
 
+const __dirname=path.resolve()
+
 app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({extended:true}));
 
@@ -28,6 +31,15 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
 app.use("/api/notifications",notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 
 
 app.listen(8000,()=>{
